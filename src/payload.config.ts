7862500f289464b30c41106741c1a8f0003164a7
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-12-19 12:47:17 +0700

    add region

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -49,6 +49,7 @@ export default buildConfig({
           adapter: s3Adapter({
             config: {
               endpoint: process.env.S3_ENDPOINT,
+              region: process.env.S3_REGION,
               credentials: {
                 accessKeyId: process.env.S3_ACCESS_KEY_ID,
                 secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
